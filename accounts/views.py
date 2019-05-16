14406a2c0edf0850ce9f446eac97012f33063f0f
hastes <rom541997@gmail.com>
2019-05-16 19:27:21 +0700

    Add facebook styles, some fixes

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from allauth.socialaccount.views import ConnectionsView
 from ads.forms import AdsCreateForm
 from ads.models import Ads
 
 
-class SocialsView(ConnectionsView):
+class SocialsView(LoginRequiredMixin, ConnectionsView):
     template_name = 'profile.html'
     success_url = reverse_lazy('get-main')
 
--- smm_tools/settings.py
+++ smm_tools/settings.py
@@ -143,9 +143,10 @@ SOCIALACCOUNT_PROVIDERS = {
     },
     'facebook': {
         'METHOD': 'oauth2',
-        'SCOPE': ['email', 'ads_management', 'ads_read', 'instagram_basic', 'manage_pages', ],
+        # 'SCOPE': ['email', 'ads_management', 'ads_read', 'instagram_basic', 'manage_pages', ],
     }
 }
 
+LOGIN_URL = '/admin/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
