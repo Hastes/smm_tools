71fa5af119d6246893e81bd11fe1aa5ee5d16cd8
hastes <rom541997@gmail.com>
2019-06-17 13:30:59 +0700

    fix(vk): Change allauth version

--- accounts/models.py
+++ accounts/models.py
@@ -58,7 +58,6 @@ class SmmUser(AbstractUser):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
-
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
